4a65904988f00d75b01834e2b71f9bb89658c8ae
Absolute Zero <ahmedwaelwanas@gmail.com>
2022-12-22 15:09:40 +0200

    cleaned code ... a bit I guess , added print

--- 1/Graph.h
+++ 1/Graph.h
@@ -3,65 +3,137 @@
 #include <iostream>
 #include <vector>
 
-class node{
-    private:
-        int id;
-        int value;
-        bool isVisited;
-    public:
-        node(int id, int val){
-            value = val;
+class Graph
+{
+private:
+    int **adjMatrix;
+
+    int getEdge(int nodeA, int nodeB)
+    {
+        return adjMatrix[nodeA][nodeB];
+    }
+
+    void insertEdge(int nodeA, int nodeB, int weight)
+    {
+        if (nodeA >= size || nodeB >= size)
+        {
+            std::cout << "please enter a valid node" << std::endl;
+            return;
         }
-};
 
-class Graph{
-    private:
-        int ** adjMatrix;
-    public:
-        int size;
+        adjMatrix[nodeA][nodeB] = weight;
+        adjMatrix[nodeB][nodeA] = weight;
+    }
 
-        Graph(int Size){
-            size = Size;
-            adjMatrix = new int*[size];
-            for(int i = 0; i < size; i++)
-                adjMatrix[i] = new int[size];
-
-            for(int i = 0 ; i < size; i++)
-                for(int j = 0; j < size; j++)
-                    adjMatrix[i][j] = 0;
-
-
+    void removeEdge(int nodeA, int nodeB)
+    {
+        if (nodeA >= size || nodeB >= size)
+        {
+            std::cout << "please enter a valid node" << std::endl;
+            return;
         }
-        int getEdge(int nodeA, int nodeB){
-            return adjMatrix[nodeA][nodeB];
-        }
-        void insertEdge(int nodeA, int nodeB, int weight){
-            if(nodeA >= size || nodeB >= size){
-                std::cout<<"please enter a valid node" << std::endl;
-                return;
-            }
+        adjMatrix[nodeA][nodeB] = 0;
+        adjMatrix[nodeB][nodeA] = 0;
+    }
 
-            adjMatrix[nodeA][nodeB] = weight;
-            adjMatrix[nodeB][nodeA] = weight;
+    int extractMinimum(int key[], bool isVisited[], int size)
+    {
+        int min = INT_MAX, currentVertex;
+        for (int v = 0; v < size; v++)
+            if (key[v] < min && isVisited[v] == false)
+                min = key[v], currentVertex = v;
 
+        return currentVertex;
+    }
+
+public:
+    int size;
+
+    Graph(int Size)
+    {
+        size = Size;
+        adjMatrix = new int *[size];
+        for (int i = 0; i < size; i++)
+            adjMatrix[i] = new int[size];
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                adjMatrix[i][j] = 0;
+    }
+
+    void primMST()
+    {
+
+        int prev[size], key[size];
+        Graph mst(size);
+        bool isVisited[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            key[i] = INT_MAX;
+            isVisited[i] = false;
         }
-        void removeEdge(int nodeA, int nodeB){
-            if(nodeA >= size || nodeB >= size){
-                std::cout<<"please enter a valid node" << std::endl;
-                return;
-            }
-            adjMatrix[nodeA][nodeB] = 0;
-            adjMatrix[nodeB][nodeA] = 0;
-        }
-        void printMatrix(){
-            for(int i = 0 ; i < size; i++){
-                for(int j = 0; j < size; j++){
-                    std::cout<<adjMatrix[i][j]<<"  ";
+        key[0] = 0;
+
+        for (int i = 0; i < size - 1; i++)
+        {
+
+            int currentVertex = extractMinimum(key, isVisited, size);
+            isVisited[currentVertex] = true;
+
+            for (int j = 0; j < size; j++)
+            {
+                if (getEdge(currentVertex, j) < key[j] &&  getEdge(currentVertex, j) != 0 && isVisited[j] == false)
+                {
+                    prev[j] = currentVertex;
+                    key[j] = getEdge(currentVertex, j);
                 }
-                std::cout<<std::endl;
             }
         }
-};
 
+        for (int i = 1; i < mst.size; i++)
+            mst.insertEdge(i, prev[i], getEdge(i, prev[i]));
+
+        mst.printMatrix();
+    }
+
+    void printMatrix()
+    {
+        std::cout << "    ";
+        for (int i = 0; i < size; i++)
+            std::cout << "v" << i + 1 << " ";
+        std::cout << std::endl;
+        for (int i = 0; i < size; i++)
+        {
+            std::cout << "v" << i + 1 << "  ";
+            for (int j = 0; j < size; j++)
+            {
+                if (adjMatrix[i][j] != 0)
+                    std::cout << adjMatrix[i][j] << "  ";
+                else
+                    std::cout << "-"
+                              << "  ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void runTestCase()
+    {
+        insertEdge(0, 1, 2);
+        insertEdge(0, 2, 4);
+        insertEdge(0, 3, 1);
+        insertEdge(1, 4, 10);
+        insertEdge(1, 3, 3);
+        insertEdge(2, 3, 2);
+        insertEdge(2, 5, 5);
+        insertEdge(5, 3, 8);
+        insertEdge(5, 6, 1);
+        insertEdge(6, 4, 6);
+        insertEdge(3, 4, 7);
+        insertEdge(6, 3, 4);
+        primMST();
+    }
+};
 
 #endif
